817864a9f952c25c291395427641c7688a5edda2
andrewdl <andrewdl@rose-hulman.edu>
2019-02-01 10:48:23 -0500

    I finished the test.

--- Exam1/src/m1_draw_problem.py
+++ Exam1/src/m1_draw_problem.py
@@ -79,7 +79,7 @@ def test_draw_a_picture():
 #       :type window: rg.RoseWindow
 #     """
 # -------------------------------------------------------------------------
-#  TODO: 1. Implement and test the draw_a_picture function.
+#  DONE: 1. Implement and test the draw_a_picture function.
 #           Tests have been written for you (above in main).
 #  We suggest breaking this into multiple commits.
 #     Can you show the right circle?
@@ -95,8 +95,16 @@ def test_draw_a_picture():
 #
 
 def draw_a_picture(point, n, color, window):
-
-    return
+    circle = rg.Circle(point, 50)
+    circle.fill_color = color
+    circle.attach_to(window)
+    for k in range(1, n+1):
+        point.x = point.x + 25
+        point.y = point.y + 15
+        newcircle = rg.Circle(rg.Point(point.x, point.y), 50)
+        newcircle.fill_color = color
+        newcircle.attach_to(window)
+    window.render(0.5)
 
 
 main()
